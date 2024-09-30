a984ca4eec3fd2bbf33e3b5f3c36a2c270aa0e6b
Muhammadahmed0012 <159796230+Muhammadahmed0012@users.noreply.github.com>
2024-09-30 15:48:36 +0300

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,37 +1,45 @@
-import Image from "next/image";
-import Link from "next/link";
+import Photo from "./(picture)/navbar/page";
 
-export default function Photo() {
+function Home() {
   return (
-    <div>
-      <Image
-        src={
-          "https://seeklogo.com/images/G/governer-sindh-logo-9968F934E2-seeklogo.com.png"
-        }
-        alt="img"
-        height={50}
-        width={50}
-      />
-      <div className="kamran_img">
-        <Image
-          src={
-            "https://portal.governorsindh.com/_next/static/media/gov_kamran.1aeb2238.png"
-          }
-          alt="kamran"
-          height={320}
-          width={350}
-        />
+    <main className="flex justify-center relative top-64">
+      <div className="id_card">
+        <div className="photo">
+          <Photo />
+        </div>
+
+        <ul>
+          <li id="li">
+            Name: <li id="li2">Muhammad Ahmad</li>
+          </li>
+          <li id="li">
+            Roll No: <li id="li2">00415333</li>
+          </li>
+          <li id="li">
+            Distance learning: <li id="li2">No</li>
+          </li>
+          <li id="li">
+            City: <li id="li2">Karachi</li>
+          </li>
+          <li id="li">
+            Center: <li id="li2">Governor House Karachi</li>
+          </li>
+          <li id="li">
+            Campus: <li id="li2">Main</li>
+          </li>
+          <li id="li">
+            Days/Time: <li id="li2">Friday-09:00 Am - 12:00 pm</li>
+          </li>
+          <li id="li">Bactch 1</li>
+          <br />
+          <p className="p">Q2</p>
+          <div className="sig">
+            <h4>Authorized Signature</h4>
+          </div>
+        </ul>
       </div>
-      <div className="my h-80 w-200">
-        <Image
-          src={
-            "https://scontent.fkhi2-2.fna.fbcdn.net/v/t39.30808-6/461392582_1083153470044934_2370783270715299469_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF39shQwuqnMPZ_7dixqM5NfVDpDjxuZbJ9UOkOPG5lsn2Z4wiYRD3h9cUPX_PQ6Q7r95PWkmdGFZ4mgLqdmsk2&_nc_ohc=E1FoX6hnTPcQ7kNvgFz2_VK&_nc_ht=scontent.fkhi2-2.fna&_nc_gid=Aciv-43IP11EAQmSVTK_Wf1&oh=00_AYAqbwipnXzie2E443xhn59PRvORaAjw-y3Fck2Mhq8Gdg&oe=670042E1"
-          }
-          alt="mypic"
-          width={130}
-          height={100}
-        />
-      </div>
-    </div>
+    </main>
   );
 }
+
+export default Home;
